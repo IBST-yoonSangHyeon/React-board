f5831aefa4be3c65360aa5a8d38246e399ede42d
IBST-yoonSangHyeon <47581425+IBST-yoonSangHyeon@users.noreply.github.com>
2021-08-16 20:46:11 +0900

    게시판 명 검색시 메인으로 이동 기능 추가

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,6 +3,9 @@ import styles from './nav.module.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 class Nav extends PureComponent {
+    handleGoToMain = () => {
+        this.props.onGoToMain();
+      }
     render() {
         return (
             <Container className="p-3 my-3 border ">
@@ -11,7 +14,7 @@ class Nav extends PureComponent {
                         <img src="/images/navLeft.png" alt="MyLeftStar" />
                     </Col>
                     <Col xs={6}>
-                        <h1 className={styles.subject}>윤상현 게시판</h1>
+                        <h1 className={styles.subject} onClick={ this.handleGoToMain}>윤상현 게시판</h1>
                     </Col>
                     <Col className="d-flex justify-content-center">
                         <img src="/images/navRight.png" alt="MyRightStar" className="" />
